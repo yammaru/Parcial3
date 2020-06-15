8635dce51878c8e116a392dab7f3d514dd3a5917
yammaru <63098405+yammaru@users.noreply.github.com>
2020-06-15 14:07:09 -0500

    Add files via upload

---
+++ Entity/Recaudado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Recaudado
+    {
+        public string Nit { get; set; }
+        public string Mes { get; set; }
+        public string Año { get; set; }
+        public string Tipo { get; set; }
+        public decimal ValorImpuesto { get; set; }
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
