1252cf61fb4c8978889737911819411cf3ac3a18
yammaru <63098405+yammaru@users.noreply.github.com>
2020-06-15 15:02:52 -0500

    Add files via upload

--- BLL/ServicioRecaudo.cs
+++ BLL/ServicioRecaudo.cs
@@ -15,13 +15,12 @@ namespace BLL
     {
         SqlConnection Connection;
         RepositorioRecaudo repositorioRecaudo;
-        Archivo archivo;
-        List<Recaudado> recaudados;
+        Archivo archivo = new Archivo();
         public ServicioRecaudo()
         {
             Connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Recaudo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             repositorioRecaudo = new RepositorioRecaudo(Connection);
-            archivo = new Archivo();
+        
         }
         public string Guardar(Recaudado recaudo)
         {
@@ -42,88 +41,54 @@ namespace BLL
         public List<Recaudado> ConsultarArchivo(string ruta) { return archivo.Consultar(ruta); }
         public List<Recaudado> Consultar()
         {
-            recaudados = new List<Recaudado>();
-            recaudados.Clear();
-            try
-            {
+          
+            
+          
                 Connection.Open();
-                recaudados = repositorioRecaudo.Consultar();
+                List<Recaudado> recaudados = repositorioRecaudo.Consultar();
                 Connection.Close();
                 return recaudados;
-            }
-            catch (Exception)
-            {
-                Connection.Close();
-                return null;
-            }
+            
         }
         public List<Recaudado> Buscar(string tipo,string mes,string año)
         {
-            recaudados = new List<Recaudado>();
-            recaudados.Clear();
-            try
-            {
+           
+            
                 Connection.Open();
-                recaudados = repositorioRecaudo.Buscar(tipo,mes,año);
+                List<Recaudado> recaudados = repositorioRecaudo.Buscar(tipo,mes,año);
                 Connection.Close();
                 return recaudados;
-            }
-            catch (Exception)
-            {
-                Connection.Close();
-                return null;
-            }
+          
         }
         public List<Recaudado> BuscarxNit_Mes_Año(string nit, string mes, string año)
         {
-            recaudados = new List<Recaudado>();
-            recaudados.Clear();
-            try
-            {
+            
+           
                 Connection.Open();
-                recaudados = repositorioRecaudo.BuscarxNit_Mes_Año(nit, mes, año);
+                List<Recaudado> recaudados = repositorioRecaudo.BuscarxNit_Mes_Año(nit, mes, año);
                 Connection.Close();
                 return recaudados;
-            }
-            catch (Exception)
-            {
-                Connection.Close();
-                return null;
-            }
+           
         }
         public List<Recaudado> BuscarxMes_Año(string mes, string año)
         {
-            recaudados = new List<Recaudado>();
-            recaudados.Clear();
-            try
-            {
+            
+           
                 Connection.Open();
-                recaudados = repositorioRecaudo.BuscarXMes_Año( mes, año);
+                List<Recaudado> recaudados = repositorioRecaudo.BuscarXMes_Año( mes, año);
                 Connection.Close();
                 return recaudados;
-            }
-            catch (Exception)
-            {
-                Connection.Close();
-                return null;
-            }
+          
         }
         public List<Recaudado> BuscarXNit(string nit)
         {
-            recaudados = new List<Recaudado>();
-            recaudados.Clear();
-            try
-            {
+            
+            
                 Connection.Open();
-                recaudados = repositorioRecaudo.BuscarXAgente(nit);
+                List<Recaudado> recaudados = repositorioRecaudo.BuscarXAgente(nit);
                 Connection.Close();
                 return recaudados;
-            }
-            catch (Exception)
-            {
-                Connection.Close();
-                return null;
-            }
+            
         }
     }
 }
